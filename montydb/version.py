9e4817a7e1abf382c8c59f23a4aa07987973ac7d
davidlatwe <david962041@gmail.com>
2018-05-15 02:31:09 +0800

    version 0.0.1

---
+++ montydb/version.py
@@ -0,0 +1,10 @@
+
+VERSION_MAJOR = 0
+VERSION_MINOR = 0
+VERSION_PATCH = 1
+
+version_info = (VERSION_MAJOR, VERSION_MINOR, VERSION_PATCH)
+version = '%i.%i.%i' % version_info
+__version__ = version
+
+__all__ = ['version', 'version_info', '__version__']
