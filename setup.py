6c20f770d832bc6ca75a6d707a95b02db701b57b
davidlatwe <david962041@gmail.com>
2018-05-22 19:02:22 +0800

    Update setup.py

--- setup.py
+++ setup.py
@@ -7,6 +7,9 @@ version_file = os.path.abspath("montydb/version.py")
 version_mod = imp.load_source("version", version_file)
 version = version_mod.version
 
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
 setup(
     name="montydb",
     version=version,
@@ -16,10 +19,30 @@ setup(
     zip_safe=True,
 
     # metadata for upload to PyPI
-    author="Lai, Ta Wei",
+    author="davidlatwe",
     author_email="david962041@gmail.com",
-    description="",
-    license="BSD 3-Clause",
+    description="A serverless Mongo-like database backed with SQLite.",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    license="BSD-3-Clause",
     keywords="",
     url="https://github.com/davidlatwe/MontyDB",
+    classifiers=(
+        "Development Status :: 1 - Planning",
+        "License :: OSI Approved :: BSD License",
+        "Intended Audience :: Developers",
+        "Topic :: Database",
+        "Topic :: Database :: Database Engines/Servers",
+        "Topic :: Utilities",
+        "Programming Language :: Python :: 2",
+        "Programming Language :: Python :: 2.7",
+        "Programming Language :: Python :: 3",
+        "Programming Language :: Python :: 3.3",
+        "Programming Language :: Python :: 3.4",
+        "Programming Language :: Python :: 3.5",
+        "Programming Language :: Python :: 3.6",
+        "Programming Language :: Python :: Implementation :: CPython",
+        "Programming Language :: Python :: Implementation :: PyPy",
+        "Operating System :: OS Independent",
+    ),
 )
