4c1f3658e26536965d1c42a5a489ab39b780cbce
davidlatwe <david962041@gmail.com>
2018-05-29 02:16:20 +0800

    Add more collection tests

--- tests/test_collection.py
+++ tests/test_collection.py
@@ -50,14 +50,44 @@ def test_collection_insert_many(monty_collection):
 
 
 def test_collection_find(monty_collection):
+    monty_collection.insert_one({"test": "doc"})
     cursor = monty_collection.find({})
     assert isinstance(cursor, MontyCursor)
 
+    db = monty_collection.database
+    assert monty_collection.name in db.collection_names()
+
+
+def test_collection_find_in_non_existed_collection(monty_collection):
+    cursor = monty_collection.find({})
+    assert isinstance(cursor, MontyCursor)
+
+    db = monty_collection.database
+    assert monty_collection.name not in db.collection_names()
+
 
 def test_collection_find_one(monty_collection):
-    result = monty_collection.find_one()
-    assert result is None
-
     monty_collection.insert_many([{"qty": 5}, {"qty": 10}])
     result = monty_collection.find_one()
     assert result["qty"] == 5
+
+
+def test_collection_find_one_in_non_existed_collection(monty_collection):
+    result = monty_collection.find_one()
+    assert result is None
+
+
+def test_collection_find_one_with_id(monty_collection):
+    monty_collection.insert_one({"_id": 1, "qty": 7})
+    result = monty_collection.find_one(1)
+    assert result["qty"] == 7
+
+
+def test_collection_get_collection(monty_collection):
+    sub_col = monty_collection.sub_col
+    assert sub_col.name == "test_col.sub_col"
+
+
+def test_collection_get_collection_faild(monty_collection):
+    with pytest.raises(AttributeError):
+        monty_collection._sub_col
