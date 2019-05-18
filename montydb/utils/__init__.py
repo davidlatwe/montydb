5c187c796d166c0201e6a59565f7d65068c15323
David Lai <david962041@gmail.com>
2019-05-19 05:04:23 +0800

    Remove MontyWalker
    Seems redundant.

--- montydb/utils/__init__.py
+++ montydb/utils/__init__.py
@@ -1,12 +1,10 @@
 
 from .io import monty_dump, monty_load
 from .mt_list import MontyList
-from .mt_walker import MontyWalker
 
 
 __all__ = [
     "monty_load",
     "monty_dump",
     "MontyList",
-    "MontyWalker",
 ]
