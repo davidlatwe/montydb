157d81cac4c5834c8d06836a63a9a61a6a40a6a3
David Lai <david962041@gmail.com>
2019-06-03 05:28:24 +0800

    Add positional projection test cases

--- tests/test_engine/test_projection/test_projection_positional.py
+++ tests/test_engine/test_projection/test_projection_positional.py
@@ -1,4 +1,9 @@
 
+import pytest
+
+from pymongo.errors import OperationFailure as mongo_op_fail
+from montydb.errors import OperationFailure as monty_op_fail
+
 
 def test_projection_positional_1(monty_proj, mongo_proj):
     docs = [
@@ -139,3 +144,199 @@ def test_projection_positional_9(monty_proj, mongo_proj):
     assert mongo_c.count() == 1
     assert monty_c.count() == mongo_c.count()
     assert next(mongo_c) == next(monty_c)
+
+
+def test_projection_positional_10(monty_proj, mongo_proj):
+    docs = [
+        {"a": {"b": [{"c": 5}, {"c": 10}], "x": [{"c": 5}, {"c": 10}]}},
+    ]
+    spec = {"a.x.c": 5}
+    proj = {"a.b.x.$": 1}
+
+    monty_c = monty_proj(docs, spec, proj)
+    mongo_c = mongo_proj(docs, spec, proj)
+
+    assert mongo_c.count() == 1
+    assert monty_c.count() == mongo_c.count()
+    assert next(mongo_c) == next(monty_c)
+
+
+def test_projection_positional_11(monty_proj, mongo_proj):
+    docs = [
+        {"a": [{"b": [0, 1, 2]}, {"b": [3, 2, 4]}]},
+    ]
+
+    def run(spec, proj):
+        monty_c = monty_proj(docs, spec, proj)
+        mongo_c = mongo_proj(docs, spec, proj)
+
+        assert mongo_c.count() == 1
+        assert monty_c.count() == mongo_c.count()
+        assert next(mongo_c) == next(monty_c)
+
+    spec = {"a.b.2": 4}
+    proj = {"a.b.$": 1}
+    run(spec, proj)
+
+    spec = {"a.b": 2}
+    proj = {"a.b.$": 1}
+    run(spec, proj)
+
+    spec = {"a.b": 2}
+    proj = {"a.$.b": 1}
+    run(spec, proj)
+
+    spec = {"a.b": 2}
+    proj = {"$.a.b": 1}
+    run(spec, proj)
+
+    spec = {"a.b": 2}
+    proj = {"a": 1, "$.a.b": 1}
+    run(spec, proj)
+
+    for ie in range(2):
+        spec = {"a.b": 2}
+        proj = {"a": ie}
+        run(spec, proj)
+
+    spec = {"a.b": 2}
+    proj = {"a.b.0.$": 1}
+    run(spec, proj)
+
+    spec = {"a.b": 2}
+    proj = {"a.b.0.1.$": 1}
+    run(spec, proj)
+
+    spec = {"a.b": 2}
+    proj = {"a.b.0.1.x.$": 1}
+    run(spec, proj)
+
+    for ie in range(2):
+        spec = {"a.b": 2}
+        proj = {"a.b.0.1.x": ie}
+        run(spec, proj)
+
+    for ie in range(2):
+        spec = {}
+        proj = {"a.0.b.x": ie}
+        run(spec, proj)
+
+        proj = {"a.b.1": ie}
+        run(spec, proj)
+
+        proj = {"a.b": ie}
+        run(spec, proj)
+
+
+def test_projection_positional_12(monty_proj, mongo_proj):
+    docs = [
+        {"a": [{"b": [{"c": 1}, {"x": 1}]}, {"b": [{"c": 1}, {"x": 1}]}]},
+
+        {"a": [{"b": [{"c": [0, 1]}, {"5": 8}, "hello", {"x": 1}, 8]},
+               {"b": [{"c": {"1": 8}}, "world", {"x": 1}, 0]}]}
+    ]
+    spec = {}
+
+    def run(proj):
+        monty_c = monty_proj(docs, spec, proj)
+        mongo_c = mongo_proj(docs, spec, proj)
+
+        assert mongo_c.count() == 2
+        assert monty_c.count() == mongo_c.count()
+        for i in range(2):
+            assert next(mongo_c) == next(monty_c)
+
+    for ie in range(2):
+        proj = {"a.b.5": ie}
+        run(proj)
+
+        proj = {"a.b.c.1": ie}
+        run(proj)
+
+        proj = {"a.x": ie}
+        run(proj)
+
+        proj = {"a.0.b": ie}
+        run(proj)
+
+        proj = {"a.b.s": ie}
+        run(proj)
+
+        proj = {"a.b.c.": ie}  # Redundant dot
+        run(proj)
+
+        proj = {"a.b.c": ie}
+        run(proj)
+
+
+def test_projection_positional_13(monty_proj, mongo_proj):
+    docs = [
+        {"a": [{"b": [1, 5]}, {"b": 2}, {"b": [3, 10, 4]}],
+         "c": [{"b": [1]}, {"b": 2}, {"b": [3, 5]}]},
+    ]
+    spec = {"a.b.0": 3, "c.b.1": 5}
+
+    def run(proj):
+        monty_c = monty_proj(docs, spec, proj)
+        mongo_c = mongo_proj(docs, spec, proj)
+
+        assert mongo_c.count() == 1
+        assert monty_c.count() == mongo_c.count()
+        assert next(mongo_c) == next(monty_c)
+
+    proj = {"a.b.$": 1}
+    run(proj)
+
+    proj = {"a.$.b": 1}
+    run(proj)
+
+
+def test_projection_positional_14(monty_proj, mongo_proj):
+    docs = [
+        {"a": 5, "b": {"c": 5, "g": 0}, "x": [1, 2]}
+    ]
+    spec = {}
+
+    def run(proj):
+        monty_c = monty_proj(docs, spec, proj)
+        mongo_c = mongo_proj(docs, spec, proj)
+
+        assert mongo_c.count() == 1
+        assert monty_c.count() == mongo_c.count()
+        assert next(mongo_c) == next(monty_c)
+
+    for ie in range(2):
+        proj = {"a": ie, "x.1": ie, "b.g": ie}
+        run(proj)
+
+
+def test_projection_positional_15(monty_proj, mongo_proj):
+    docs = [
+        {"a": [{"b": [0, 1, {"c": 5}]}, {"b": [3, 2, {"x": 5}]}]},
+    ]
+    spec = {"a.b.1": 1}
+    proj = {"a.b.$": 1, "a.b.x": 1}
+
+    monty_c = monty_proj(docs, spec, proj)
+    mongo_c = mongo_proj(docs, spec, proj)
+
+    assert mongo_c.count() == 1
+    assert monty_c.count() == mongo_c.count()
+    assert next(mongo_c) == next(monty_c)
+
+
+def test_projection_positional_err_2(monty_proj, mongo_proj):
+    docs = [
+        {"a": [{"b": [0, 1, 2]}, {"b": [3, 2, 4]}],
+         "b": [{"b": [0, 1, 2]}, {"b": [3, 2, 4]}]}
+    ]
+    spec = {"b.0.b": 2}  # a.b not matched with a.0
+    proj = {"a.b.$": 1}
+
+    with pytest.raises(mongo_op_fail) as mongo_err:
+        next(mongo_proj(docs, spec, proj))
+
+    with pytest.raises(monty_op_fail) as monty_err:
+        next(monty_proj(docs, spec, proj))
+
+    assert mongo_err.value.code == monty_err.value.code
