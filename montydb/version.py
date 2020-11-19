ed4fd33d7ef1558189566883093c30070aebf253
David Lai <davidlatwe@gmail.com>
2020-11-20 02:10:17 +0800

    version bump

--- montydb/version.py
+++ montydb/version.py
@@ -1,7 +1,7 @@
 
 VERSION_MAJOR = 2
-VERSION_MINOR = 1
-VERSION_PATCH = 1
+VERSION_MINOR = 2
+VERSION_PATCH = 0
 
 version_info = (VERSION_MAJOR, VERSION_MINOR, VERSION_PATCH)
 version = "%i.%i.%i" % version_info
