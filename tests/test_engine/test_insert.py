a788e8b8fc7eb9224634c397c95843aae0ce0169
David Lai <davidlatwe@gmail.com>
2021-06-07 23:23:10 +0800

    add insert bytes test

--- tests/test_engine/test_insert.py
+++ tests/test_engine/test_insert.py
@@ -30,3 +30,15 @@ def test_insert_invalid_doc_2(monty_find, mongo_find):
 
     with pytest.raises(errors.InvalidDocument):
         monty_find(docs, spec)
+
+
+def test_insert_bytes(monty_find, mongo_find):
+    docs = [
+        {"data": b"some bytes"},
+    ]
+    spec = {}
+
+    monty_c = monty_find(docs, spec)
+    mongo_c = mongo_find(docs, spec)
+
+    assert next(monty_c) == next(mongo_c)
