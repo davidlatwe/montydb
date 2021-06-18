931a5cb216112e0e07f4f154629298e5363e423c
Michael Currin <18750745+MichaelCurrin@users.noreply.github.com>
2021-06-18 16:55:47 +0200

    refactor: update database.py

--- montydb/database.py
+++ montydb/database.py
@@ -5,8 +5,8 @@ from .types import string_types, encode_
 
 
 class MontyDatabase(BaseObject):
+
     def __init__(self, client, name, codec_options=None, write_concern=None):
-        """ """
         super(MontyDatabase, self).__init__(
             codec_options or client.codec_options, write_concern or client.write_concern
         )
@@ -18,6 +18,7 @@ class MontyDatabase(BaseObject):
     def __eq__(self, other):
         if isinstance(other, self.__class__):
             return self._client == other.client and self._name == other.name
+      
         return NotImplemented
 
     def __ne__(self, other):
@@ -40,13 +41,12 @@ class MontyDatabase(BaseObject):
     @property
     def name(self):
         """
-        The name of this Database.
+        Return the name of this Database.
         """
         return self._name
 
     @property
     def client(self):
-        """ """
         return self._client
 
     def collection_names(self):
@@ -56,43 +56,49 @@ class MontyDatabase(BaseObject):
 
     def create_collection(self, name, codec_options=None, write_concern=None, **kwargs):
         """
-        Create a collection, before any insertion,
-        raise error if exists.
+        Create a collection or raise an error if it already exists.
         """
         if self.client._storage.collection_exists(self, name):
             error_msg = "collection {} already exists".format(encode_(name))
+      
             raise errors.CollectionInvalid(error_msg)
-        else:
-            collection = self.get_collection(
-                name, codec_options, write_concern, **kwargs
-            )
-            self.client._storage.collection_create(self, name)
-            return collection
+        
+        collection = self.get_collection(
+            name, codec_options, write_concern, **kwargs
+        )
+        self.client._storage.collection_create(self, name)
+       
+        return collection
 
     def drop_collection(self, name_or_collection):
-        """ """
         name = name_or_collection
+
         if isinstance(name_or_collection, MontyCollection):
             name = name_or_collection.name
         elif not isinstance(name_or_collection, string_types):
             raise TypeError("name_or_collection must be an instance of basestring")
+        
         self.client._storage.collection_drop(self, name)
 
     def get_collection(self, name, codec_options=None, write_concern=None, **kwargs):
         """
-        Get a collection.
+        Get a collection and throw an error for invalid name.
         """
-        # verify collection name
-        is_invaild = False
-        for invaild_char in ("$", "\0", "\x00"):
-            if invaild_char in name:
-                is_invaild = True
-        if name.startswith("system."):
-            is_invaild = True
+        if not name:
+            raise errors.OperationFailure("Invalid collection name - it may not be blank")
 
-        if is_invaild or not name:
-            raise errors.OperationFailure("Invalid collection name.")
-        else:
-            return MontyCollection(
-                self, name, False, codec_options, write_concern, **kwargs
-            )
+        for c in ("$", "\0", "\x00"):
+            if c in name:
+                raise errors.OperationFailure("Invalid collection name containing special characters: {}".format(name))
+
+        if name.startswith("system."):
+            raise errors.OperationFailure("Invalid prefix in collection name: {}".format(name))
+        
+        return MontyCollection(
+            self, 
+            name, 
+            False, 
+            codec_options, 
+            write_concern, 
+            **kwargs
+        )
