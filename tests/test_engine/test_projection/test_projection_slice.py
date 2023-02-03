016b62a1a24a8875a684c03b1b37783207b563a5
davidlatwe <davidlatwe@gmail.com>
2023-02-03 19:18:28 +0800

    Cleanup

--- tests/test_engine/test_projection/test_projection_slice.py
+++ tests/test_engine/test_projection/test_projection_slice.py
@@ -147,9 +147,7 @@ def test_projection_slice_fields_with_regular_inclusion(monty_proj, mongo_proj):
 
     assert count_documents(mongo_c, spec) == 1
     assert count_documents(monty_c, spec) == count_documents(mongo_c, spec)
-    res = next(mongo_c)
-    print(res)
-    assert res == next(monty_c)
+    assert next(mongo_c) == next(monty_c)
 
 
 def test_projection_slice_fields_with_id_exclusion(monty_proj, mongo_proj):
